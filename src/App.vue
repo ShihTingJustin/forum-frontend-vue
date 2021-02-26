8145136937f928669d1076f66c8d628aae7910b5
Justin Huang <justinhuang777@gmail.com>
2021-02-27 00:44:58 +0800

    import bootstrap and delete useless file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="forum">
+    <router-view />
   </div>
 </template>
 
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+export default {
+  name: "App",
+};
+</script>
+
 <style>
-#app {
+#forum {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
