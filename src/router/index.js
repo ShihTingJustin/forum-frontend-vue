19d1cfac90942bd64a64ba5465cd70e4ce07063e
Justin Huang <justinhuang777@gmail.com>
2021-02-27 14:42:54 +0800

    add SignIn and SignUp page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import NotFound from '../views/NotFound'
+import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
   {
     path: '*',
     name: 'not-found',
