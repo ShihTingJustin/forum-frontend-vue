277477da6319722971cf47e5f0450656bb644ea8
Justin Huang <justinhuang777@gmail.com>
2021-02-27 18:43:09 +0800

    add pages and routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Forum from '../views/Forum.vue'
 
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/forum'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -16,6 +22,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/forum',
+    name: 'forum',
+    component: Forum
+  },
+  {
+    path: '/forum/feed',
+    name: 'forum-feeds',
+    component: () => import('../views/Feeds.vue')
+  },
+  {
+    path: '/forum/top',
+    name: 'forum-top',
+    component: () => import('../views/Top.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',
