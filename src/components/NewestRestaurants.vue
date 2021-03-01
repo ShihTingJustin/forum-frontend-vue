30d21a20af4aff8831dcc4648fa3cdeb91b5bac1
Justin Huang <justinhuang777@gmail.com>
2021-03-01 11:11:37 +0800

    add NewestRestaurants component and dummyData

---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <div class="card-header">最新餐廳</div>
+    <div class="card-body">
+      <div v-for="restaurant in restaurants" :key="restaurant.id">
+        <h4>
+          <a href="#">{{ restaurant.title }}</a>
+          <small>{{ restaurant.Category ? restaurant.Category.name : '未分類' }}</small>
+        </h4>
+        <p>
+          {{ restaurant.description }}
+        </p>
+        {{ restaurant.createdAt }}
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    restaurants: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
