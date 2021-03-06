9a0248e66741c100b4451e9a5c23ec049c3f7946
Justin Huang <justinhuang777@gmail.com>
2021-03-06 17:48:39 +0800

    add RestaurantComments component

---
+++ src/components/RestaurantComments.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2 class="my-4">
+      所有評論：
+    </h2>
+
+    <div 
+      v-for="comment in restaurantComments"
+      :key="comment.id"
+    >
+      <blockquote class="blockquote mb-0">
+        <button
+          type="button"
+          class="btn btn-danger float-right"
+        >
+          Delete
+        </button>
+        <h3>
+          <a href="#">
+            {{ comment.User.name }}
+          </a>
+        </h3>
+        <p>{{ comment.text }}</p>
+        <footer class="blockquote-footer">
+          {{ comment.createdAt }}
+        </footer>
+      </blockquote>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RestaurantComments",
+  props: {
+    restaurantComments: {
+      type: Array,
+      require: true
+    }
+  }
+}
+</script>
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container py-5">
-    <RestaurantDetail 
-    :initial-restaurant="restaurant"
-    />
+    <RestaurantDetail :initial-restaurant="restaurant"/>
+    <RestaurantComments :restaurant-comments="restaurantComments" />
   </div>
 </template>
 
 <script>
 import RestaurantDetail from "./../components/RestaurantDetail";
+import RestaurantComments from "./../components/RestaurantComments";
 
 const dummyData = {
   restaurant: {
@@ -49,6 +49,25 @@ const dummyData = {
           updatedAt: "2019-06-23T01:16:31.000Z",
         },
       },
+      {
+        id: 3,
+        text: "Quos asperiores in nostrum cupiditate excepturi aspernatur.",
+        UserId: 2,
+        RestaurantId: 1,
+        createdAt: "2019-06-22T09:00:43.000Z",
+        updatedAt: "2019-06-22T09:00:43.000Z",
+        User: {
+          id: 2,
+          name: "user1",
+          email: "user1@example.com",
+          password:
+            "$2a$10$0ISHJI48xu/VRNVmEeycFe8v5ChyT305f8KaJVIhumu7M/eKAikkm",
+          image: "https://i.imgur.com/XooCt5K.png",
+          isAdmin: false,
+          createdAt: "2019-06-22T09:00:43.000Z",
+          updatedAt: "2019-06-23T01:16:31.000Z",
+        },
+      }
     ],
   },
   isFavorited: false,
@@ -59,6 +78,7 @@ export default {
   name: "Restaurant",
   components: {
     RestaurantDetail,
+    RestaurantComments
   },
   data() {
     return {
